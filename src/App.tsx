fce0c8ac1b305c398e1b63ba377d447d1baa5683
Justin Suh <justindjsuh@gmail.com>
2023-03-16 17:29:00 -0400

    added foundational html

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
@@ -8,9 +6,43 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Receipt Calculator</h1>
+      <div className="selector__container">
+        <button>Split Evenly</button>
+        <button>Manual Entry</button>
+      </div>
       <div className="bill__container"></div>
-      <div className="tip__container"></div>
-      <div className="total__container"></div>
+      <div className="details__container">
+        <div className="tip__container">
+          <div className="tip__header">
+            <p>Tip</p>
+            <p>{}</p>
+          </div>
+          <div className="tip__slider">
+            <input
+              type="range"
+              min="0"
+              max="30"
+              value="20"
+              className="slider"
+            />
+          </div>
+        </div>
+        <div className="people__container">
+          <div className="people__header">
+            <p>People</p>
+            <p>{}</p>
+          </div>
+          <div className="people__slider">
+            <input type="range" min="2" max="20" value="2" className="slider" />
+          </div>
+        </div>
+        <hr />
+        <div className="total_split__container">
+          <p>Split total</p>
+          <p>{}</p>
+        </div>
+      </div>
     </div>
   );
 }
